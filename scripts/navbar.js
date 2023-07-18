56b6dff70d2b817d6c04f5d5c5a5c58ce25c95f4
Nguyen Tuan Anh <tuannt3999@gmail.com>
2023-07-18 08:56:20 +0700

    Fixed navigation action

---
+++ scripts/navbar.js
@@ -0,0 +1,9 @@
+// const navbarList = document.querySelector(".bot-navigation__list");
+// let prevActiveItem;
+
+// navbarList?.addEventListener("click", function (e) {
+// 	if (prevActiveItem) prevActiveItem.classList.remove("active");
+// 	const navItem = e.target.closest(".bot-navigation__item");
+// 	prevActiveItem = navItem;
+// 	navItem.classList.add("active");
+// });
